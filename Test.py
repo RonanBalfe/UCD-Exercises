1f410066b8fec2d1754ddc0aa171b3dc8a80aee4
RonanBalfe <Ronanbalfe@gmail.com>
2021-06-08 10:25:19 +0100

    Initialised project. Added main , test and csv

---
+++ Test.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import pandas as pd
+import numpy as np
+data=pd.read_csv("BankChurners.csv")
+print(data)
+print(data["CLIENTNUM"])
+print(data.describe())
+print(data.tail(6))
+
