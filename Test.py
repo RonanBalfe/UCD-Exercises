c63d3f1ebd5f6f6789cb130208569d43d1c47297
RonanBalfe <Ronanbalfe@gmail.com>
2021-06-30 22:09:14 +0100

    test update.

--- Test.py
+++ Test.py
@@ -1,11 +1,12 @@
-import pandas as pd
-import pandas as pd
-import numpy as np
-data=pd.read_csv("BankChurners.csv")
-print(data)
-print(data["CLIENTNUM"])
-print(data.describe())
-print(data.tail(6))
 
-1+1
-print(1+1)
+"All we need for API is these 4 lines"
+
+
+import requests
+
+data=requests.get("http://api.open-notify.org/astros.json")
+parsed_data=data.json()
+print(parsed_data)
+
+for i in parsed_data["people"]:
+    print(i["name"])
