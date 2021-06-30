6f4cb5ca5fe06bbe0a0d93ff54ef45a806203c90
RonanBalfe <Ronanbalfe@gmail.com>
2021-06-30 16:19:11 +0100

    test update.

---
+++ Project.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import numpy as np
+
+deposits = pd.read_csv("/Users/Balfe/PycharmProjects/UCD Exercises/Data/database.csv")
+print(deposits)
+
+print(deposits.describe())
+print(deposits.head(5))
+print(deposits.tail(5))
+print(deposits.shape)
+
+
+missing_values_count = deposits.isnull().sum()
+print(missing_values_count[0:20])
+
+cleaned_deposits =deposits.fillna(0)
+missing_values_count = cleaned_deposits.isnull().sum()
+print(missing_values_count[0:20])
+
+
+
